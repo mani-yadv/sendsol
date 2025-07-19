b1ae05fe108d093e9125b0821cd83daf3914c044
mani-yadv <maniyadv@icloud.com>
2025-07-19 23:03:48 +0000

    test: add test file to verify public folder serving
    - Add test.txt to verify if public folder works at all
    - If /.well-known/test.txt serves correctly, then public folder works
    - This will help isolate if issue is with specific files or entire public serving
    
    Test URL: https://sendsol.app/.well-known/test.txt

--- components/common/LogoMain.vue
+++ components/common/LogoMain.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="logo flex items-center space-x-2">
         <NuxtLink href="/" class="flex items-center space-x-2">
-            <NuxtImg src="/logo.png" alt="SendSol Logo" class="size-7 rounded-lg" />
+            <NuxtImg src="/img/logo.png" alt="SendSol Logo" class="size-7 rounded-lg" />
             <span class="mb-0.5 text-2xl font-medium">SENDSOL</span>
         </NuxtLink>
     </div>
